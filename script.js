8c845313df3d31f0d040c1c4bf3e294d071c8a10
Eligio93 <eligio.cristantielli@gmail.com>
2023-06-29 12:33:07 +1000

    add createBoard function in module

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
 let playgrid=document.getElementById("playgrid");
 
 let gameBoard=(()=>{
-    let gameArray=["X","O","X","O","X","O","X","O","X"];
+    let gameArray=["","","","","","","","",""];
+    
+    const createBoard=(array)=>{
+        for(let i=0;i < array.length;i++){
+            const cell=document.createElement("button");
+            cell.classList.add("cell");
+            cell.setAttribute("data-cell", i);
+            cell.textContent=array[i];
+            playgrid.appendChild(cell);
+        }
+    }
+
     return {
-        gameArray
+        gameArray,
+        createBoard
     }
 
 })();
-function createBoard(array){
-    for(let i=0;i < array.length;i++){
-        const cell=document.createElement("div");
-        cell.classList.add("cell");
-        cell.setAttribute("data-cell", i);
-        cell.textContent=array[i];
-        playgrid.appendChild(cell);
+
+const player=(name,mark,status)=>{
+
+    return {
+        name,mark
     }
 }
-createBoard(gameBoard.gameArray);
+
+
+
+
+
+
+
+const player1= player("Eligio","X","On");
+const player2= player("Marco","O","Off");
+
+gameBoard.createBoard(gameBoard.gameArray);
